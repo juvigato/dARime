ba73349b3c568de0a66fb1292c9657cd99880125
Guilherme Enes <guilherme.epecci@gmail.com>
2020-02-18 16:18:04 -0300

    Adicionado botoes Play, Pass e Check.

--- Who ARe you?/ViewController.swift
+++ Who ARe you?/ViewController.swift
@@ -16,12 +16,11 @@ class ViewController: UIViewController, ARSessionDelegate, ARSCNViewDelegate {
     var currentFaceAnchor: ARFaceAnchor?
     var contentNode = SCNNode()
     var face:SCNNode = SCNNode()
-    var buttonT = UIButton()
+    var playButton = UIButton()
+    var checkButton = UIButton()
+    var passButton = UIButton()
     
     let node = SCNNode()
-
-    
-    var needChange = true
 
 
     override func viewDidLoad() {
@@ -51,26 +50,59 @@ class ViewController: UIViewController, ARSessionDelegate, ARSCNViewDelegate {
         let viewHeight = self.view.frame.height
         let viewWidth = self.view.frame.width
         
-        buttonT.frame = CGRect(x: viewWidth/1.60, y: viewHeight/2 + 200, width: 160 , height: 80)
-        buttonT.backgroundColor = .orange
+        playButton.frame = CGRect(x: viewWidth/3.2, y: viewHeight/2 + 200, width: 160 , height: 80)
+        playButton.backgroundColor = .orange
         
-        buttonT.addTarget(self, action: #selector(handleButton(_:)), for: .touchDown)
+        playButton.addTarget(self, action: #selector(playButtonRecognizer(_:)), for: .touchDown)
 
-        sceneView.addSubview(buttonT)
+        sceneView.addSubview(playButton)
 
     }
     
-    @objc func handleButton(_ gestureRecognize: UIGestureRecognizer){
-        if needChange {
-            buttonT.addTarget(self, action: #selector(handleButton(_:)), for: .touchDown)
-
-            needChange = false
-            face.removeFromParentNode()
-            face = Random3DNodes().random3DPicker()
-            node.addChildNode(face)
-        } else {
-            needChange = true
-        }
+    func checkButtonFunc() {
+        let viewHeight = self.view.frame.height
+        let viewWidth = self.view.frame.width
+        
+        passButton.frame = CGRect(x: viewWidth/1.6, y: viewHeight/2 + 200, width: 160 , height: 80)
+        passButton.backgroundColor = .orange
+        
+        passButton.addTarget(self, action: #selector(handleCheckButton(_:)), for: .touchDown) //vai realizar uma ação quando tocado
+        
+        sceneView.addSubview(passButton)
+    }
+    
+    func passButtonFunc() {
+        let viewHeight = self.view.frame.height
+        let viewWidth = self.view.frame.width
+        
+        checkButton.frame = CGRect(x: viewWidth/500, y: viewHeight/2 + 200, width: 160 , height: 80)
+        checkButton.backgroundColor = .orange
+        
+        checkButton.addTarget(self, action: #selector(handlePassButton(_:)), for: .touchDown) //vai realizar uma ação quando tocado
+        
+        sceneView.addSubview(checkButton)
+    }
+    
+    
+    @objc func handleCheckButton(_ gestureRecognize: UIGestureRecognizer){ //Quando clica em acerto, faz algo
+        face.removeFromParentNode()
+        face = Random3DNodes().random3DPicker()
+        node.addChildNode(face)
+    }
+    
+    @objc func handlePassButton(_ gestureRecognize: UIGestureRecognizer){ // QUando clica em passar, faz algo
+        
+    }
+    
+    
+    
+    
+    
+    
+    @objc func playButtonRecognizer(_ gestureRecognize: UIGestureRecognizer){
+            checkButtonFunc()
+            passButtonFunc()
+            playButton.removeFromSuperview()
     }
     
     
